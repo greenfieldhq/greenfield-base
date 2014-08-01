01aa488f798d65ba5826c26b994d26b89d22dc24
Ryan Tremaine <ryan.trem@gmail.com>
2014-08-01 13:15:01 -0400

    started implementing transloadit.

---
+++ ember/app/components/transloadit.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+var TransloaditComponent = Ember.Component.extend({
+  didInsertElement: function() {
+    this._super();
+  }
+});
+
+export default TransloaditComponent;
