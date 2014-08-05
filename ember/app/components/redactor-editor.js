68f5fe6e9dcd51adb019f9dae4f4805d85971a51
Ryan Tremaine <ryan.trem@gmail.com>
2014-08-05 16:07:20 -0400

    Fixed binding to redactor in component.

--- ember/app/components/redactor-editor.js
+++ ember/app/components/redactor-editor.js
@@ -1,16 +1,34 @@
 import Ember from 'ember';
 
-var RedactorEditorComponent = Ember.Component.extend({
-  actions: {
-    save: function() {
-      this.sendAction();
+var RedactorEditorComponent = Ember.Component.extend({ 
+  tagName: 'div',
+  init: function() {
+    this._super();
+
+    this.on("focusOut", this, this._elementValueDidChange);
+    this.on("change", this, this._elementValueDidChange);
+    this.on("paste", this, this._elementValueDidChange);
+    this.on("cut", this, this._elementValueDidChange);
+    this.on("input", this, this._elementValueDidChange);
+  },
+  _updateElementValue: Ember.observer(function() {
+    var $el, value;
+    value = Ember.get(this, "content.body");
+    $el = this.$().context;
+    if ($el && value !== $el.innerHTML) {
+      return $el.innerHTML = value;
     }
+  }, "value"),
+  _elementValueDidChange: function() {
+    var $el;
+    $el = this.$().context;
+    return Ember.set(this, "content.body", $el.innerHTML);
   },
   didInsertElement: function() {
-    this._super();
-    Ember.$('.redactor_content').redactor({
-        air: true
+    this.$().redactor({
+      air: true
     });
+    this._updateElementValue();
   }
 });
 
