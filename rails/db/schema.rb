3ff3d454fd84b3a71272711ff6c55ee0e7f1a746
Ryan Tremaine <ryan.trem@gmail.com>
2014-09-06 09:16:45 -0400

    Add reset password token

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801191945) do
+ActiveRecord::Schema.define(version: 20140819144552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20140801191945) do
   end
 
   create_table "documents", force: true do |t|
-    t.integer  "user_id",    null: false
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
@@ -53,6 +52,8 @@ ActiveRecord::Schema.define(version: 20140801191945) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
