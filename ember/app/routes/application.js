9e0d6760ec7d37446be3e37cf4e9f8635df3186e
Dave Kroondyk <davekaro@gmail.com>
2014-11-17 16:04:26 -0500

    add error messages to login page

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  actions: {
+    sessionAuthenticationFailed: function(response) {
+      // response comes from the rejecetd promise in app/initializers/custom-authenticator.js
+      // when `authenticate` method fails
+      this.controllerFor('login').set('loginErrorMessage', response.errors[0]);
+    }
+  }
+});
